11f82bc5688f2c2e8af731e284a73bcd823c546e
thermogl <info@thermoglobalnuclearwar.com>
2019-12-04 21:57:13 +0000

    improve output

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const shell = require('shelljs')
 
 try {
   const style = core.getInput('style');
-  console.log(`Using style ${style}!`);
+  console.log(`Using style: ${style}`);
   const time = Math.round((new Date()).getTime() / 1000);
   core.setOutput("build-number", time);
   shell.exec(`xcrun agvtool new-version -all ${time}`);
